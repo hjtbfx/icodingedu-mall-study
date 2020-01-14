f320dcba163571281b9cc901f9840b8b561b41ac
gavin <huangjingtao0302@126.com>
2020-01-14 13:36:21 +0800

    统一错误输出需要放在业务module里

---
+++ im-product-manage/src/main/java/com/icodingedu/mall/product/tools/MyExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.icodingedu.mall.product.tools;
+
+import com.icodingedu.mall.common.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class MyExceptionHandler {
+
+    //Exception异常处理，也可以细化到具体的异常
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    //这里将统一返回结合在一起用
+    public R handleException(Exception e){
+        //这里的code可以根据业务需要自行编写
+        return R.returnFAIL(500,e.getMessage());
+    }
+}
