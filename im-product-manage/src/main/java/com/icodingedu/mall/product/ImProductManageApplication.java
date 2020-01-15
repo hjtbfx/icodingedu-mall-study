bb9490fde53fbbb671aeed0e91993004e5c2bb36
gavin <huangjingtao0302@126.com>
2020-01-15 13:10:59 +0800

    将公共包中的类扫入应用中

--- im-product-manage/src/main/java/com/icodingedu/mall/product/ImProductManageApplication.java
+++ im-product-manage/src/main/java/com/icodingedu/mall/product/ImProductManageApplication.java
@@ -2,8 +2,10 @@ package com.icodingedu.mall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@ComponentScan("com.icodingedu.mall")
 @SpringBootApplication
 @MapperScan("com.icodingedu.mall.product.mapper")
 public class ImProductManageApplication {
